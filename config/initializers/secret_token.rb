cccdac3dd276370647c5eb6a4cd29689e785067f
jeffcode <jm4828@nyu.edu>
2014-03-04 18:28:12 -0500

    Add Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails::Application.config.secret_key_base = 'a3f78bcc44074517a7a38fb656ae5a0fdfcd4cd8e1bb4830fe42639ae398a606dcf49fd7885b172c0b58c2b3e8c43c452606598e99c7c867501a0f5e62750458'
